2e2e9fe37ae28a260b3cc1db4a6af90ccf40502e
vidal james cubero <cubero.vj@gmail.com>
2022-06-24 21:28:47 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const track = document.querySelector(".ugc-cate-comp-cards");
+let initialPosition = null;
+let initialPositionY = null;
+let moving = false;
+let transform = 0;
+
+const gestureStart = (e) => {
+    initialPosition = e.pageX;
+    initialPositionY = e.pageY;
+    moving = true;
+    const transformMatrix = window.getComputedStyle(track).getPropertyValue('transform')
+    
+    if (transformMatrix !== "none") {
+        transform = parseInt(transformMatrix.split(',')[4].trim());
+   
+    }
+}
+
+const gestureMove = (e) => {
+    if (moving) {
+        const currentPosition = e.pageX;
+        const currentPositionY = e.pageY;
+        const diff = currentPosition - initialPosition;
+        let transformValue = transform + diff;
+
+        if (transform + diff > 0) {
+            transformValue = 0
+        } 
+        
+        if (transform + diff < track.offsetWidth - 853.5){
+            transformValue = track.offsetWidth - 853.5
+        } 
+
+        track.style.transform = `translateX(${transformValue}px)`;
+
+        if (screen.width < 768) {
+            let heightValue =initialPositionY - currentPositionY;
+            window.scrollBy(0, heightValue);
+        }
+    }
+}
+
+const gestureStop = (e) => {
+    moving = false;
+}
+
+track.addEventListener('mousedown', gestureStart)
+
+track.addEventListener('mousemove', gestureMove)
+
+track.addEventListener('mouseup', gestureStop)
+
+track.addEventListener('touchdown', gestureStart)
+
+track.addEventListener('touchmove', gestureMove)
+
+track.addEventListener('touchup', gestureStop)
+
+track.addEventListener('pointerdown', gestureStart)
+
+track.addEventListener('pointermove', gestureMove)
+
+track.addEventListener('pointerup', gestureStop)
+
